8c9dfdf9b30d06200968180e94c958f62fdf5ad6
Perttu Ranta-aho <perttu.ranta-aho@f-secure.com>
2021-04-23 19:34:45 +0300

    Two solutions for problem 7

--- src/leetcode.jl
+++ src/leetcode.jl
@@ -1,6 +1,6 @@
 module leetcode
 
-export problem1
+export problem1, problem7a, problem7b
 
 """
     problem1(nums::Vector{Int}, trgt::Int)
@@ -33,6 +33,69 @@ function problem1(nums::Vector{Int}, trgt::Int)
         for (idx2, val2) in enumerate(nums[idx1+1:end])
             val1 + val2 == trgt && return idx1 .+ [0 idx2]
         end
+    end
+end  # problem1
+
+
+"""
+    problem7a(x::Int)
+
+Given a signed 32-bit integer ``x``, return ``x`` with its digits reversed. If reversing
+``x`` causes the value to go outside the signed 32-bit integer range ``[-2^{31}, 2^{31} -
+1]``, then return 0.
+
+a-variant is solution with integer arithmetics.
+
+# Examples
+```jldoctest; setup = :(using leetcode)
+julia> problem7a(123)
+321
+
+julia> problem7a(-123)
+-321
+````
+"""
+function problem7a(x::Int)
+    sg = sign(x)
+    if x < 0
+        x = -x
+    end
+    inverse = 0
+    max_int = 2^31 - 1
+    while x > 0
+        inverse = inverse * 10 + x % 10
+        x = x ÷ 10
+        if inverse > max_int
+            return 0
+        end
+    end
+    inverse = inverse * sg
+end  # problem7a
+
+
+"""
+    problem7b(x::Int)
+
+Given a signed 32-bit integer ``x``, return ``x`` with its digits reversed. If reversing
+``x`` causes the value to go outside the signed 32-bit integer range ``[-2^{31}, 2^{31} -
+1]``, then return 0.
+
+b-variant is solution with string casting.
+
+# Examples
+```jldoctest; setup = :(using leetcode)
+julia> problem7b(123)
+321
+
+julia> problem7b(-123)
+-321
+````
+"""
+function problem7b(x::Int)
+    try
+        sign(x) * parse(Int32, reverse(string(abs(x))))
+    catch
+        0
     end
 end
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,8 +3,32 @@ using Test, Documenter
 
 @testset verbose = true "Problem1 tests" begin
     doctest(leetcode, manual=false)
-    @test leetcode.problem1([2,7,11,15], 9) == [1 2]
-    @test leetcode.problem1([2,7,11,15], 13) == [1 3]
-    @test leetcode.problem1([6,3,2,4], 6) == [3 4]
-    @test leetcode.problem1([3,3], 6) == [1 2]
+    @test problem1([2,7,11,15], 9) == [1 2]
+    @test problem1([2,7,11,15], 13) == [1 3]
+    @test problem1([6,3,2,4], 6) == [3 4]
+    @test problem1([3,3], 6) == [1 2]
 end
+
+
+@testset verbose = true "Problem7 tests" begin
+
+    @testset verbose = true "a-variant" begin
+        doctest(leetcode, manual=false)
+        @test problem7a(123) == 321
+        @test problem7a(120) == 21
+        @test problem7a(0) == 0
+        @test problem7a(-456) == -654
+        @test problem7a(1123456789) == 0
+    end
+
+    @testset verbose = true "b-variant" begin
+        doctest(leetcode, manual=false)
+        @test problem7b(123) == 321
+        @test problem7b(120) == 21
+        @test problem7b(0) == 0
+        @test problem7b(-456) == -654
+        @test problem7b(1123456789) == 0
+    end
+end
+
+
